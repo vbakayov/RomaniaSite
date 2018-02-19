d0aab512e74d7441e78e5e10a2f87bd15f3ac0b9
vbakayov <viktorbakayov@gmail.com>
2018-02-19 19:29:45 +0100

    start translation

--- Resources/js/global.js
+++ Resources/js/global.js
@@ -1,39 +1,80 @@
 // Global Javascript Initialization
 var Global = function() {
   'use strict';
+    //select the buttons
+    var buttons = {};
+    buttons[0] = document.getElementById( 'pokrivki' );
+    buttons[1]= document.getElementById( 'vulneniDebeliTsvetni' );
+    buttons[2] = document.getElementById('pokrivki_chervena');
+    buttons[3] = document.getElementById('debeli_estestveni');
+    buttons[4] = document.getElementById('pokrivki_transilvanska');
+    buttons[5] = document.getElementById('pokrivki_bqla');
+    buttons[6] = document.getElementById('pokrivki_zelena');
+    buttons[7] = document.getElementById('pokrivki_oranjeva');
+    buttons[8] = document.getElementById('pokrivki_ekru');
+    buttons[9] = document.getElementById('pokrivki_sinia');
+    buttons[10] = document.getElementById('pokrivki_karirana');
+    buttons[11] = document.getElementById('pokrivki_all');
+    buttons[12] = document.getElementById('pamuk_kare');
+    buttons[13] = document.getElementById('pamuk_jakard');
+    buttons[14] = document.getElementById('olekoteni');
+    buttons[15] = document.getElementById('agneshki');
+    buttons[16] = document.getElementById('vuzglavnici');
+    buttons[17] = document.getElementById('torbichki');
+    buttons[18] = document.getElementById('all');
+    buttons[19] = document.getElementById('service'); //drugi
+
+    var text_container = document.getElementById('productText');
+    var dunavski = ['pokrivki_bqla', 'pokrivki_zelena', 'pokrivki_oranjeva', 'pokrivki_ekru', 'pokrivki_sinia', 'pokrivki_chervena'];
 
 
-    // var menuLeft = document.getElementById( 'pokrivki' ),
-    //     menuRight = document.getElementById( 'vulneniDebeliTsvetni' ),
-    //     elementText = document.getElementById('productText'),
-    //     allBox = document.getElementById('all');
-    //
-    //
-    //
-    // console.log(menuLeft);
-    // allBox.addEventListener("click", function() {click(allBox.id)} );
-    // menuLeft.addEventListener("click", function() {click(menuLeft.id)} );
-    // menuRight.addEventListener("click", function() {click(menuRight.id)} );
-    //
-    // function click(elementId)
-    // {
-    //      // me.addClass('cbp-filter-item-active').siblings().removeClass('cbp-filter-item-active');
-    //     console.log(elementId);
-    //      if (elementId === "pokrivki")
-    //     {
-    //         elementText.innerText = "pokrivkiTekst";
-    //
-    //
-    //     }
-    //     else if (elementId === "vulneniDebeli")
-    //     {
-    //         elementText.innerText = "vulneniDebeliTekst";
-    //
-    //     }
-    //     else if(elementId === "all"){
-    //          // jQuery('#all').addClass('cbp-filter-item-active');
-    //      }
-    // }
+    //add event listeners
+    for (var k = 0; k < Object.keys(buttons).length; k++) {
+        (function(){
+            console.log(buttons[k]);
+            buttons[k].addEventListener('click', click, false);
+        })();
+    }
+
+    //handle click
+    function click(event)
+    {
+      load_text(this.id);
+    }
+
+
+
+    //change text
+    function load_text(elementId){
+        console.log("clicked");
+
+        if (elementId === "pokrivki_all")
+        {
+            text_container.innerText = "Fețele de masă cu motive populare sunt împodobite cu motive populare tradiționale românești și balcanice. Am elaborat două modele de fețe de masă tradiționale – modelul Dunărean și cel Transilvănean. Motivele decorative ale modelului dunărean sunt caracteristice pentru România de sud - Valahia (Muntenia și Oltenia). " +
+                "Modelul transilvănean este  inspirat de motivele tradiționale din Transilvania. Fețele de masă sunt bine acceptate atât de străinii care vizitează țară, încât și de românii. Clienții principali ai companiei noastre sunt comercianții orientați către comerțul cu suvenire și restaurantele tradiționale.";
+        }
+        else if(elementId === "pokrivki_transilvanska"){
+            text_container.innerText = "Ștergarul transilvănean este inspirat de motivele tradiționale din Transilvania."
+
+        }
+        else if (dunavski.indexOf(elementId)> -1){
+            text_container.innerText = "Motivele decorative ale modelului dunărean sunt caracteristice pentru România de sud - Valahia ( Muntenia și Oltenia). ";
+        }
+        else if (elementId === "pokrivki_karirana"){
+            text_container.innerText = "Careul este un motiv care poate fi găsit în toate desenurilor populare încă din timpurile vechi.";
+        }
+        else if (elementId === "vulneniDebeli")
+        {
+            text_container.innerText = "vulneniDebeliTekst";
+            console.log("vulneni debeli clicked clicked");
+
+        }
+        else if(elementId === "all"){
+            // jQuery('#all').addClass('cbp-filter-item-active');
+        }
+
+
+    }
 
   // Bootstra Components
   var handleBootstrapComponents = function() {
@@ -145,7 +186,7 @@ var Global = function() {
       if(parent.attr('data-related')) {
         $(parent.attr('data-related')).css('height', parent.height());
       }
-   });       
+   });
   }
 
   return {
